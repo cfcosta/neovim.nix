3fa953c8b5038b3bbafdf2f704a91aa990da4fbc
Cainã Costa <me@cfcosta.com>
2023-02-18 22:51:45 -0300

    feat(nix): add option for installing neovide + devshell

--- flake.nix
+++ flake.nix
@@ -1,26 +1,39 @@
 {
   description =
-    "An attempt to make neovim cli functional like an IDE while being very beautiful, blazing fast startuptime ";
+    "An attempt to make neovim cli functional like an IDE while being very beautiful, blazing fast startuptime";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { nixpkgs, ... }: {
-    hmModule = { options, config, lib, pkgs, ... }:
-      let cfg = config.programs.devos.neovim;
-      in {
-        options.programs.devos.neovim.enable =
-          lib.mkEnableOption "NVChad Configuration";
+  outputs = { nixpkgs, flake-utils, ... }:
+    {
+      hmModule = { options, config, lib, pkgs, ... }:
+        let cfg = config.programs.devos.neovim;
+        in {
+          options.programs.devos.neovim.enable =
+            lib.mkEnableOption "NVChad Configuration";
+          opttions.programs.devos.neovim.neovide.enable =
+            lib.mkEnableOption "Neovide Intagration";
 
-        config = lib.mkIf cfg.enable {
-          home.packages = with pkgs; [ tree-sitter ];
+          config = lib.mkIf cfg.enable {
+            home.packages = with pkgs;
+              [ tree-sitter ] ++ lib.optionals cfg.neovide.enable [ neovide ];
 
-          programs.neovim.enable = true;
+            programs.neovim.enable = true;
 
-          xdg.configFile."nvim" = {
-            source = ./.;
-            recursive = true;
+            xdg.configFile."nvim" = {
+              source = ./.;
+              recursive = true;
+            };
           };
         };
-      };
-  };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ lua-language-server ];
+        };
+      });
 }

bc03898474c394d4d39cb438a468a375342c133c
Cainã Costa <me@cfcosta.com>
2023-03-18 22:22:59 -0300

    fix: remove chatgpt.nvim
    That was a failed experiment.

--- lua/core/lazy_load.lua
+++ lua/core/lazy_load.lua
@@ -60,7 +60,6 @@ M.packer_cmds = {
 
 M.treesitter_cmds = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo" }
 M.mason_cmds = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" }
-M.chatgpt_cmds = { "ChatGPT", "ChatGPTActAs" }
 
 M.gitsigns = function()
   autocmd({ "BufRead" }, {

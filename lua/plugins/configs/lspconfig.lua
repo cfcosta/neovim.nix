23c638e08421637ef45d02c8ae19d74fb183c08c
Cainã Costa <me@cfcosta.com>
2023-05-25 23:22:05 -0300

    feat: enable format on save using null-ls

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -24,16 +24,14 @@ M.on_attach = function(client, bufnr)
   end
 
   -- format buffer on save
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.formatting_sync()
-      end,
-    })
-  end
+  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.formatting_sync()
+    end,
+  })
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()

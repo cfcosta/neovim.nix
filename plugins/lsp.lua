62385fc8deba501ee4c0959c860e53f637e885ec
Cainã Costa <me@cfcosta.com>
2023-05-26 15:48:33 -0300

    fix: setup lsp directly instead of trying to infer

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -19,18 +19,15 @@ return {
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     require("mason").setup()
-    require("mason-lspconfig").setup()
-    require("mason-lspconfig").setup_handlers {
-      function(server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {
-          capabilities = capabilities,
-        }
-      end,
-      ["rust_analyzer"] = function()
-        require("rust-tools").setup {
-          capabilities = capabilities,
-        }
-      end,
+    require("mason-lspconfig").setup {
+      ensure_installed = { "lua_ls", "rust_analyzer" },
+      handlers = {
+        ["rust_analyzer"] = function()
+          require("rust-tools").setup {
+            capabilities = capabilities,
+          }
+        end,
+      },
     }
 
     local cmp = require "cmp"

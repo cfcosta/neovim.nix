3c18d6710da3b3c77313b4d5e9ef28f030adb1b3
Cainã Costa <me@cfcosta.com>
2023-06-02 11:16:27 -0300

    feat: set default indentation level and config

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 vim.g.mapleader = " "
 
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {

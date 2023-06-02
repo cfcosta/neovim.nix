26bf809580f4a433a11c798bc873584ce7f5c451
Cainã Costa <me@cfcosta.com>
2023-06-02 15:46:03 -0300

    feat: enable installing lsp from the editor flake
    Idea is to remove mason in the and, relying only on the flake
    definition.

--- flake.nix
+++ flake.nix
@@ -5,16 +5,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
     {
       hmModule = { options, config, lib, pkgs, ... }:
         let cfg = config.programs.devos.neovim;
         in {
           options.programs.devos.neovim = {
             enable = lib.mkEnableOption "My Neovim configuration";
-            neovide.enable = lib.mkEnableOption "Neovide Support";
+
+            lsp.modules = lib.mkOption {
+              type = lib.types.listOf lib.types.string;
+              default = [ "nix" "rust" "lua" "sh" "js" ];
+              description = "The LSP modules to enable";
+            };
           };
 
           config = lib.mkIf cfg.enable {
@@ -27,7 +40,26 @@
                 ripgrep
                 tree-sitter
                 vimPlugins.nvim-treesitter.withAllGrammars
-              ] ++ lib.optionals cfg.neovide.enable [ neovide ];
+              ] ++ (lib.optionals (builtins.elem "rust" cfg.lsp.modules) [
+                (rust-overlay.packages.${pkgs.system}.default.override {
+                  extensions = [ "rust-analyzer" "rust-src" "rustfmt" ];
+                })
+              ]) ++ (lib.optionals (builtins.elem "nix" cfg.lsp.modules) [
+                pkgs.rnix-lsp
+                pkgs.nixfmt
+              ]) ++ (lib.optionals (builtins.elem "lua" cfg.lsp.modules) [
+                pkgs.lua-language-server
+                pkgs.stylua
+              ]) ++ (lib.optionals (builtins.elem "sh" cfg.lsp.modules) [
+                pkgs.nodePackages.bash-language-server
+                pkgs.shellcheck
+                pkgs.shfmt
+              ]) ++ (lib.optionals (builtins.elem "js" cfg.lsp.modules) [
+                pkgs.nodePackages.dockerfile-language-server-nodejs
+                pkgs.nodePackages.typescript-language-server
+                pkgs.nodePackages.vscode-json-languageserver
+                pkgs.nodePackages.yaml-language-server
+              ]);
 
             programs.neovim.enable = true;
 

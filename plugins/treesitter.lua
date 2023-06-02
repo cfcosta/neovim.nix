1029e7f35d59b98b954c10a56aa3d7188fed1556
Cainã Costa <me@cfcosta.com>
2023-06-02 10:55:33 -0300

    feat: enable treesitter-aware endwise

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,11 +1,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "RRethy/nvim-treesitter-endwise",
+  },
   config = function()
     require("nvim-treesitter.configs").setup {
       ensure_installed = { "rust", "nix" },
       sync_install = false,
       auto_install = true,
       highlight = { enable = true },
+      endwise = {
+        enable = true,
+      },
     }
   end,
 }

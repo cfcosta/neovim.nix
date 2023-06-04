d1bfa23d0ebecbd383e8b16e785c6cf611f002fd
Cainã Costa <me@cfcosta.com>
2023-06-04 18:17:02 -0300

    feat: remove old config
    We are moving to a new config based on
    [nightvim](https://github.com/cfcosta/nightvim).

--- plugins/terminal.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  event = "VeryLazy",
-  config = true,
-  keys = {
-    { "<leader>ot", "<cmd>ToggleTerm direction=horizontal<cr>" },
-    { "<leader>oT", "<cmd>ToggleTerm direction=vertical<cr>" },
-  },
-}

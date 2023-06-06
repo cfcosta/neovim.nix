07019ea821f70da0ff11b86dbe7c840f048ce8d8
Cainã Costa <me@cfcosta.com>
2023-06-06 11:09:17 -0300

    feat: add indent_blankline

--- flake.nix
+++ flake.nix
@@ -140,6 +140,10 @@
       url = "github:Wansmer/treesj";
       flake = false;
     };
+    indent-blankline = {
+      url = "github:lukas-reineke/indent-blankline.nvim";
+      flake = false;
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, home-manager, nightvim, ... }:
@@ -169,7 +173,7 @@
             (nightvim.lib.mkPlugin "diffview" diffview { })
             (nightvim.lib.mkPlugin "gitsigns" gitsigns { })
             (nightvim.lib.mkPlugin "neo-tree" neo-tree {
-              depends = [ "plenary" "nvim-web-devicons" "nui" ];
+              depends = [ "plenary" "nvim-web-devicons" "nui" "moonlight" ];
               config = ''
                 vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
                 require("neo-tree").setup()
@@ -441,6 +445,40 @@
               '';
             })
             (nightvim.lib.mkPlugin "treesj" treesj { })
+            (nightvim.lib.mkPlugin "indent-blankline" indent-blankline {
+              config = ''
+                vim.cmd [[highlight IndentBlanklineIndent1 guifg=#282D45 gui=nocombine]]
+                vim.cmd [[highlight IndentBlanklineIndent2 guifg=#2F3552 gui=nocombine]]
+                vim.cmd [[highlight IndentBlanklineIndent3 guifg=#373D5E gui=nocombine]]
+                vim.cmd [[highlight IndentBlanklineIndent4 guifg=#3E466B gui=nocombine]]
+                vim.cmd [[highlight IndentBlanklineIndent5 guifg=#464E78 gui=nocombine]]
+                vim.cmd [[highlight IndentBlanklineIndent6 guifg=#4D5685 gui=nocombine]]
+
+                require("indent_blankline").setup {
+                  filetype_exclude = {
+                    "help",
+                    "terminal",
+                    "lspinfo",
+                    "TelescopePrompt",
+                    "TelescopeResults",
+                    "",
+                  },
+                  buftype_exclude = { "terminal" },
+                  show_trailing_blankline_indent = false,
+                  show_first_indent_level = false,
+                  show_current_context = true,
+                  show_current_context_start = true,
+                  char_highlight_list = {
+                    "IndentBlanklineIndent1",
+                    "IndentBlanklineIndent2",
+                    "IndentBlanklineIndent3",
+                    "IndentBlanklineIndent4",
+                    "IndentBlanklineIndent5",
+                    "IndentBlanklineIndent6",
+                  },
+                }
+              '';
+            })
           ];
         };
       };

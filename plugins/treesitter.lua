4cef67c4df4825be880d1349f73635f57cd3f309
Cainã Costa <me@cfcosta.com>
2023-06-20 15:13:56 -0500

    chore: extract plugin config into their own files

---
+++ plugins/treesitter.lua
@@ -0,0 +1,18 @@
+local parser_install_dir = vim.fn.stdpath "cache" .. "/treesitters"
+vim.fn.mkdir(parser_install_dir, "p")
+vim.opt.runtimepath:append(parser_install_dir)
+
+require("nvim-treesitter.configs").setup {
+  ensure_installed = { "rust", "nix" },
+  sync_install = false,
+  auto_install = true,
+  highlight = { enable = true },
+  endwise = { enable = true },
+  indent = { enable = true },
+  parser_install_dir = parser_install_dir,
+}
+
+-- Enable folding using treesitter
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false

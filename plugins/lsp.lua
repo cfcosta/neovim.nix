a5691d4b6d86dbdbd22d7dad3059b85eb046740d
Cainã Costa <me@cfcosta.com>
2023-06-26 13:43:21 -0300

    feat: add go support

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -23,3 +23,21 @@ lspconfig.lua_ls.setup {
     },
   },
 }
+
+lspconfig.gopls.setup {
+  cmd = { "gopls" },
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+  settings = {
+    gopls = {
+      experimentalPostfixCompletions = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+  init_options = {
+    usePlaceholders = true,
+  },
+}

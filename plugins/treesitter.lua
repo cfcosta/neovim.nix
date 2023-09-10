802e5676146e18ebbee635250a306055127ceb11
Cainã Costa <me@cfcosta.com>
2023-09-10 17:35:08 -0300

    fix: add more treesitter parsers to always install

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,16 +1,16 @@
-local parser_install_dir = vim.fn.stdpath "cache" .. "/treesitters"
+local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
 vim.fn.mkdir(parser_install_dir, "p")
 vim.opt.runtimepath:append(parser_install_dir)
 
-require("nvim-treesitter.configs").setup {
-  ensure_installed = { "rust", "nix" },
-  sync_install = false,
-  auto_install = true,
-  highlight = { enable = true },
-  endwise = { enable = true },
-  indent = { enable = true },
-  parser_install_dir = parser_install_dir,
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "rust", "nix", "lua", "c", "help", "vim" },
+	sync_install = false,
+	auto_install = true,
+	highlight = { enable = true },
+	endwise = { enable = true },
+	indent = { enable = true },
+	parser_install_dir = parser_install_dir,
+})
 
 -- Enable folding using treesitter
 vim.opt.foldmethod = "expr"

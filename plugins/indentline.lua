20a500b910506c4121a2724b4ee4309cffc3d149
Cainã Costa <me@cfcosta.com>
2023-09-28 11:33:19 -0300

    chore: update indent-blankline config to version 3

--- plugins/indentline.lua
+++ plugins/indentline.lua
@@ -1,30 +1,38 @@
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#282D45 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guifg=#2F3552 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent3 guifg=#373D5E gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent4 guifg=#3E466B gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent5 guifg=#464E78 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#4D5685 gui=nocombine]])
+local highlight = {
+  "IBL1",
+  "IBL2",
+  "IBL3",
+  "IBL4",
+  "IBL5",
+  "IBL6",
+}
 
-require("indent_blankline").setup({
-  filetype_exclude = {
-    "help",
-    "terminal",
-    "lspinfo",
-    "TelescopePrompt",
-    "TelescopeResults",
-    "",
-  },
-  buftype_exclude = { "terminal" },
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = false,
-  show_current_context = true,
-  show_current_context_start = true,
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-    "IndentBlanklineIndent3",
-    "IndentBlanklineIndent4",
-    "IndentBlanklineIndent5",
-    "IndentBlanklineIndent6",
+local hooks = require("ibl.hooks")
+
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "IBL1", { fg = "#282D45" })
+  vim.api.nvim_set_hl(0, "IBL2", { fg = "#2F3552" })
+  vim.api.nvim_set_hl(0, "IBL3", { fg = "#373D5E" })
+  vim.api.nvim_set_hl(0, "IBL4", { fg = "#3E466B" })
+  vim.api.nvim_set_hl(0, "IBL5", { fg = "#464E78" })
+  vim.api.nvim_set_hl(0, "IBL6", { fg = "#4D5685" })
+end)
+
+-- Hide first level of indenting
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+
+require("ibl").setup({
+  indent = { highlight = highlight },
+  exclude = {
+    filetype = {
+      "help",
+      "terminal",
+      "lspinfo",
+      "TelescopePrompt",
+      "TelescopeResults",
+      "",
+    },
   },
 })

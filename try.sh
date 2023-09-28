533bbb98f3f2ca515d36eec0424d7ea971b9cdf6
Cainã Costa <me@cfcosta.com>
2023-09-28 11:36:00 -0300

    chore: update try.sh to be the same as test.sh

--- try.sh
+++ try.sh
@@ -20,20 +20,30 @@ trap cleanup INT
 
 which nix &>/dev/null || die "Nix is not on path"
 
-NIX="$(which nix) \
+ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+cd "${ROOT}" || exit 1
+
+NIX="nix \
   --extra-experimental-features nix-command \
   --extra-experimental-features flakes"
 
 ${NIX} build --out-link "${TEMP}/result" github:cfcosta/neovim.nix
 
+cd - || exit 1
+
 mkdir -p "${TEMP}/.local/share"
 mkdir -p "${TEMP}/.cache"
 
+export HOME="${TEMP}"
 export XDG_CONFIG_HOME="${TEMP}/result/home-files/.config"
 export XDG_CACHE_HOME="${TEMP}/.cache"
 export XDG_DATA_HOME="${TEMP}/.local/share"
 export PATH="${TEMP}/result/home-path/bin:${PATH}"
 
-"${TEMP}/result/home-path/bin/nvim"
+# Disable some plugins that require user credentials like wakatime
+export NVIM_CONFIG_IN_TEST="true"
+
+"${TEMP}/result/home-path/bin/nvim" $@
 
 cleanup

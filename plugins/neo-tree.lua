d27817cfd9286d2ef0aa04ae7b8e9e5c2d70bf9f
Cainã Costa <me@cfcosta.com>
2024-07-31 11:07:36 -0300

    chore: add some adjustments to neotree

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,16 +1,20 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 require("neo-tree").setup({
+  window = {
+    position = "left",
+    width = 32,
+  },
   filesystem = {
+    hijack_netrw_behavior = "open_current",
+    use_libuv_file_watcher = true,
+
     filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = true,
+
       always_show = {
-        ".dockerignore",
         ".env",
-        ".envrc",
-        ".github",
-        ".gitignore",
-        ".gitlab-ci.yml",
-        ".justfile",
       },
     },
   },

2f16726068b806990f3583332aa717a451fb3ef6
Cainã Costa <me@cfcosta.com>
2024-07-31 11:08:32 -0300

    chore: remove unused tree-sitter parsers

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,24 +5,9 @@ local new_runtimepath = vim.opt.runtimepath:get()
 table.insert(new_runtimepath, 1, parser_install_dir)
 vim.opt.runtimepath = new_runtimepath
 
--- Add support for just files
-require("nvim-treesitter.install").compilers = { "gcc", "clang" }
-require("nvim-treesitter.parsers").get_parser_configs().just = {
-  install_info = {
-    url = "https://github.com/IndianBoy42/tree-sitter-just",
-    files = { "src/parser.c", "src/scanner.cc" },
-    branch = "main",
-    use_makefile = true, -- this may be necessary on MacOS (try if you see compiler errors)
-  },
-  maintainers = { "@IndianBoy42" },
-}
-
 require("nvim-treesitter.configs").setup({
   ensure_installed = {
     "bash",
-    "c",
-    "cmake",
-    "cpp",
     "css",
     "csv",
     "dockerfile",
@@ -31,12 +16,8 @@ require("nvim-treesitter.configs").setup({
     "gitattributes",
     "gitcommit",
     "gitignore",
-    "go",
-    "gomod",
-    "gosum",
     "html",
     "json",
-    "just",
     "llvm",
     "lua",
     "markdown",

bdcbd938e0fa568646e42e3031a56089423cecb9
Cainã Costa <me@cfcosta.com>
2024-08-25 11:21:18 -0300

    chore: configure rust-analyzer correctly

--- plugins/rustacean.lua
+++ plugins/rustacean.lua
@@ -1,3 +1,34 @@
 vim.g.rustaceanvim = function()
-  return {}
+  return {
+    server = {
+      cmd = { "rust-analyzer" },
+
+      settings = {
+        ["rust-analyzer"] = {
+          cargo = {
+            buildScripts = { enable = true },
+            allTargets = false,
+          },
+          hover = {
+            actions = {
+              implementations = true,
+              references = true,
+              run = true,
+            },
+          },
+          imports = { preferPrelude = true },
+          inlayHints = {
+            maxLength = nil,
+            lifetimeElisionHints = {
+              useParameterNames = true,
+              enable = "skip_trivial",
+            },
+            closureReturnTypeHints = { enable = "always" },
+          },
+          procMacro = { enable = true },
+          rust = { analyzerTargetDir = true },
+        },
+      },
+    },
+  }
 end

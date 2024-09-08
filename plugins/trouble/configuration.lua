d300a9d1cf181ccc356f7e82fe716ea76a0f0ec8
Cainã Costa <me@cfcosta.com>
2024-09-08 20:11:54 -0300

    fix(trouble): commands changed name

--- plugins/trouble/configuration.lua
+++ plugins/trouble/configuration.lua
@@ -1,8 +1,7 @@
 require("trouble").setup({})
 
 map("n", "<leader>xx", "<cmd>Trouble<cr>")
-map("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>")
-map("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>")
-map("n", "<leader>xq", "<cmd>Trouble quicklist<cr>")
-map("n", "<leader>xl", "<cmd>Trouble loclist<cr>")
-map("n", "gr", "<cmd>Trouble lsp_references<cr>")
+map("n", "<leader>xd", "<cmd>Trouble diagnostics<cr>")
+map("n", "<leader>xl", "<cmd>Trouble lsp_command<cr>")
+map("n", "gr", "<cmeTrouble lsp_references<cr>")
+map("n", "gd", "<cmeTrouble lsp_definition<cr>")

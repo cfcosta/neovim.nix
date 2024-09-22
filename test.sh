9abf08beb4c28a4ad1c40b81f0c40d74850be9c3
Cainã Costa <me@cfcosta.com>
2024-09-22 14:39:20 -0300

    fix: update test scripts to use the package

--- test.sh
+++ test.sh
@@ -2,45 +2,19 @@
 
 set -e
 
-TEMP="$(mktemp -d)"
-
 die() {
-	local error="${1:-Not Specified}"
+  local error="${1:-Not Specified}"
 
-	echo "ERROR: ${error}"
-	exit 1
+  echo "ERROR: ${error}"
+  exit 1
 }
-
-cleanup() {
-	rm -rf "${TEMP}"
-	exit 0
-}
-
-trap cleanup INT
 
 which nix &>/dev/null || die "Nix is not on path"
 
 ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-cd "${ROOT}" || exit 1
-
 NIX="nix \
   --extra-experimental-features nix-command \
   --extra-experimental-features flakes"
 
-${NIX} build --out-link "${TEMP}/result"
-
-cd - || exit 1
-
-mkdir -p "${TEMP}/.local/share"
-mkdir -p "${TEMP}/.cache"
-
-export HOME="${TEMP}"
-export XDG_CONFIG_HOME="${TEMP}/result/home-files/.config"
-export XDG_CACHE_HOME="${TEMP}/.cache"
-export XDG_DATA_HOME="${TEMP}/.local/share"
-export PATH="${TEMP}/result/home-path/bin:${PATH}"
-
-"${TEMP}/result/home-path/bin/nvim" "$@"
-
-cleanup
+${NIX} run "${ROOT}#"

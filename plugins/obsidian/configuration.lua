1466b51b1ad93cedc4cdcd004d192eb3cf8f08b6
Cainã Costa <me@cfcosta.com>
2024-10-06 13:09:51 -0300

    chore(obsidian): add new keybindings

--- plugins/obsidian/configuration.lua
+++ plugins/obsidian/configuration.lua
@@ -7,3 +7,13 @@ require("obsidian").setup({
     },
   },
 })
+
+vim.keymap.set("n", "<leader>nT", "<cmd> ObsidianTomorrow<CR>", { desc = "obsidian: open journal entry for tomorrow" })
+vim.keymap.set("n", "<leader>ns", "<cmd> ObsidianSearch<CR>", { desc = "obsidian: search notes" })
+vim.keymap.set("n", "<leader>nt", "<cmd> ObsidianToday<CR>", { desc = "obsidian: open journal entry for today" })
+vim.keymap.set(
+  "n",
+  "<leader>ny",
+  "<cmd> ObsidianYesterday<CR>",
+  { desc = "obsidian: open journal entry for yesterday" }
+)
--- plugins/telescope/configuration.lua
+++ plugins/telescope/configuration.lua
@@ -41,7 +41,7 @@ local options = {
     },
     find_command = { "rg", "--files", "--hidden", "--ignore-vcs" },
     file_sorter = sorters.get_fuzzy_file,
-    file_ignore_patterns = { "node_modules", "%.git" },
+    file_ignore_patterns = { "node_modules", "%.git", ".obsidian", ".stfolder", ".trash", ".versions" },
     generic_sorter = sorters.get_generic_fuzzy_sorter,
     path_display = { "truncate" },
     winblend = 0,

214033a00cf913a7a801faa0e897da18b38dde26
Cainã Costa <me@cfcosta.com>
2024-12-03 12:02:25 -0300

    feat: load plugins dinamically

--- plugins/default.nix
+++ plugins/default.nix
@@ -72,126 +72,119 @@ in
 symlinkJoin rec {
   name = "nightvim-plugins";
 
-  paths = [
-    (importPlugin ./autopairs)
-    (importPlugin ./avante)
-    (importPlugin ./catppuccin)
-    (importPlugin ./cmp)
-    (importPlugin ./conform)
-    (importPlugin ./dropbar)
-    (importPlugin ./fix-cursor-hold.nix)
-    (importPlugin ./grug-far)
-    (importPlugin ./indent-blankline)
-    (importPlugin ./lspconfig)
-    (importPlugin ./lualine)
-    (importPlugin ./neogit)
-    (importPlugin ./neotest)
-    (importPlugin ./nio.nix)
-    (importPlugin ./notify.nix)
-    (importPlugin ./nvim-tree)
-    (importPlugin ./nvim-web-devicons)
-    (importPlugin ./obsidian)
-    (importPlugin ./octo)
-    (importPlugin ./render-markdown)
-    (importPlugin ./rustacean)
-    (importPlugin ./surround)
-    (importPlugin ./telescope)
-    (importPlugin ./telescope-ui-select.nix)
-    (importPlugin ./toggleterm)
-    (importPlugin ./treesitter)
-    (importPlugin ./treesj.nix)
-    (importPlugin ./trouble)
-    (importPlugin ./wakatime)
-    (importPlugin ./zen-mode)
+  paths =
+    let
+      inherit (builtins) readDir;
 
-    (mkPlugin {
-      name = "aiken-neovim";
-      src = inputs.aiken-neovim;
-      config = "";
-    })
-    (mkPlugin {
-      name = "cmp-buffer";
-      src = inputs.cmp-buffer;
-      config = "";
-    })
-    (mkPlugin {
-      name = "cmp-cmdline";
-      src = inputs.cmp-cmdline;
-      config = "";
-    })
-    (mkPlugin {
-      name = "cmp-nvim-lsp";
-      src = inputs.cmp-nvim-lsp;
-      config = "";
-    })
-    (mkPlugin {
-      name = "cmp-path";
-      src = inputs.cmp-path;
-      config = "";
-    })
-    (mkPlugin {
-      name = "cmp-snippy";
-      src = inputs.cmp-snippy;
-      config = "";
-    })
-    (mkPlugin {
-      name = "comment";
-      src = inputs.comment;
-      module = "Comment";
-    })
-    (mkPlugin {
-      name = "diffview";
-      src = inputs.diffview;
-    })
-    (mkPlugin {
-      name = "dressing";
-      src = inputs.dressing;
-      config = "";
-    })
-    (mkPlugin {
-      name = "gitsigns";
-      src = inputs.gitsigns;
-    })
-    (mkPlugin {
-      name = "lspkind";
-      src = inputs.lspkind;
-      config = "";
-    })
-    (mkPlugin {
-      name = "nui";
-      src = inputs.nui;
-      config = "";
-    })
-    (mkPlugin {
-      name = "nvim-dap";
-      src = inputs.nvim-dap;
-      config = "";
-    })
-    (mkPlugin {
-      name = "nvim-snippy";
-      src = inputs.nvim-snippy;
-      config = "";
-    })
-    (mkPlugin {
-      name = "nvim-treesitter-endwise";
-      src = inputs.nvim-treesitter-endwise;
-      config = "";
-    })
-    (mkPlugin {
-      name = "nvim-treesitter-just";
-      src = inputs.nvim-treesitter-just;
-      config = "";
-    })
-    (mkPlugin {
-      name = "plenary";
-      src = inputs.plenary;
-      config = "";
-    })
-    (mkPlugin {
-      name = "which-key";
-      src = inputs.which-key;
-    })
-  ];
+      dir = ./.;
+      entries = readDir dir;
+
+      importPluginPath =
+        name:
+        let
+          path = dir + "/${name}";
+          type = (readDir dir).${name};
+        in
+        if type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix" then
+          importPlugin (dir + "/${name}")
+        else if type == "directory" then
+          importPlugin path
+        else
+          null;
+
+      # Filter out non-plugin entries and import all valid plugins
+      plugins = lib.filter (x: x != null) (map importPluginPath (builtins.attrNames entries));
+    in
+    plugins
+    ++ [
+      (mkPlugin {
+        name = "aiken-neovim";
+        src = inputs.aiken-neovim;
+        config = "";
+      })
+      (mkPlugin {
+        name = "cmp-buffer";
+        src = inputs.cmp-buffer;
+        config = "";
+      })
+      (mkPlugin {
+        name = "cmp-cmdline";
+        src = inputs.cmp-cmdline;
+        config = "";
+      })
+      (mkPlugin {
+        name = "cmp-nvim-lsp";
+        src = inputs.cmp-nvim-lsp;
+        config = "";
+      })
+      (mkPlugin {
+        name = "cmp-path";
+        src = inputs.cmp-path;
+        config = "";
+      })
+      (mkPlugin {
+        name = "cmp-snippy";
+        src = inputs.cmp-snippy;
+        config = "";
+      })
+      (mkPlugin {
+        name = "comment";
+        src = inputs.comment;
+        module = "Comment";
+      })
+      (mkPlugin {
+        name = "diffview";
+        src = inputs.diffview;
+      })
+      (mkPlugin {
+        name = "dressing";
+        src = inputs.dressing;
+        config = "";
+      })
+      (mkPlugin {
+        name = "gitsigns";
+        src = inputs.gitsigns;
+      })
+      (mkPlugin {
+        name = "lspkind";
+        src = inputs.lspkind;
+        config = "";
+      })
+      (mkPlugin {
+        name = "nui";
+        src = inputs.nui;
+        config = "";
+      })
+      (mkPlugin {
+        name = "nvim-dap";
+        src = inputs.nvim-dap;
+        config = "";
+      })
+      (mkPlugin {
+        name = "nvim-snippy";
+        src = inputs.nvim-snippy;
+        config = "";
+      })
+      (mkPlugin {
+        name = "nvim-treesitter-endwise";
+        src = inputs.nvim-treesitter-endwise;
+        config = "";
+      })
+      (mkPlugin {
+        name = "nvim-treesitter-just";
+        src = inputs.nvim-treesitter-just;
+        config = "";
+      })
+      (mkPlugin {
+        name = "plenary";
+        src = inputs.plenary;
+        config = "";
+      })
+      (mkPlugin {
+        name = "which-key";
+        src = inputs.which-key;
+      })
+    ];
 
   passthru.specs = map (p: p.spec) paths;
 }

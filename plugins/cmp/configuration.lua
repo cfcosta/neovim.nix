8e894b56dd283e2b38830f94061b259874f20a74
Cainã Costa <me@cfcosta.com>
2024-12-18 21:22:06 -0300

    chore(cmp): use lspkind icons

--- plugins/cmp/configuration.lua
+++ plugins/cmp/configuration.lua
@@ -1,43 +1,11 @@
 local cmp = require("cmp")
 local compare = require("cmp.config.compare")
 
-local kind_icons = {
-  Text = "",
-  Method = "󰆧",
-  Function = "󰊕",
-  Constructor = "",
-  Field = "󰇽",
-  Variable = "󰂡",
-  Class = "󰠱",
-  Interface = "",
-  Module = "",
-  Property = "󰜢",
-  Unit = "",
-  Value = "󰎠",
-  Enum = "",
-  Keyword = "󰌋",
-  Snippet = "",
-  Color = "󰏘",
-  File = "󰈙",
-  Reference = "",
-  Folder = "󰉋",
-  EnumMember = "",
-  Constant = "󰏿",
-  Struct = "",
-  Event = "",
-  Operator = "󰆕",
-  TypeParameter = "󰅲",
-}
-
 cmp.setup({
   snippet = {
     expand = function(args)
       require("snippy").expand_snippet(args.body)
     end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -58,16 +26,7 @@ cmp.setup({
     { name = "buffer" },
   }),
   formatting = {
-    format = function(entry, vim_item)
-      -- Kind icons
-      -- This concatenates the icons with the name of the item kind
-      vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
-      -- Source
-      vim_item.menu = ({
-        avante = "󱗻",
-      })[entry.source.name]
-      return vim_item
-    end,
+    format = require("lspkind").cmp_format(),
   },
   sorting = {
     priority_weight = 2,

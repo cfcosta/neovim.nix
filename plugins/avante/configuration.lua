63ab0636b687a9799c7dc0df378fea5efb2dc681
Cainã Costa <me@cfcosta.com>
2025-03-08 14:27:19 -0300

    feat(avante): disable auto suggestions

--- plugins/avante/configuration.lua
+++ plugins/avante/configuration.lua
@@ -3,19 +3,17 @@ require("avante_lib").load()
 require("avante").setup({
   provider = "claude",
   auto_suggestions_provider = "openai",
-
   claude = {
     model = "claude-3-7-sonnet-latest",
   },
   behaviour = {
-    auto_suggestions = os.getenv("OPENROUTER_API_KEY") ~= nil,
+    auto_suggestions = false,
     auto_set_highlight_group = true,
     auto_set_keymaps = true,
     auto_apply_diff_after_generation = false,
     support_paste_from_clipboard = false,
     minimize_diff = true,
   },
-
   mappings = {
     suggestion = {
       accept = "<Tab>",

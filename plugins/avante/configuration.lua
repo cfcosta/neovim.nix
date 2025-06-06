03b622405a970fe43e1a937134ba02e5a46af62e
Cainã Costa <me@cfcosta.com>
2025-06-06 11:56:44 -0300

    chore(avante): remove customized options
    It wasn't working well since the plugin has been changed to be an
    agent-like thing a few months ago.

--- plugins/avante/configuration.lua
+++ plugins/avante/configuration.lua
@@ -4,15 +4,7 @@ require("avante").setup({
   provider = "claude",
   auto_suggestions_provider = "openai",
   claude = {
-    model = "claude-3-7-sonnet-latest",
-  },
-  behaviour = {
-    auto_suggestions = false,
-    auto_set_highlight_group = true,
-    auto_set_keymaps = true,
-    auto_apply_diff_after_generation = false,
-    support_paste_from_clipboard = false,
-    minimize_diff = true,
+    model = "claude-opus-4-20250514",
   },
   mappings = {
     suggestion = {

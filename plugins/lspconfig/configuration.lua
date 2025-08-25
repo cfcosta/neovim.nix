3342cae2da1a2faf3c1a1b9f29aa0a2453f4235b
Cainã Costa <me@cfcosta.com>
2025-08-24 23:30:33 -0300

    chore: fix deprecation warning for diagnostics

--- plugins/lspconfig/configuration.lua
+++ plugins/lspconfig/configuration.lua
@@ -102,12 +102,18 @@ lspconfig.pyright.setup({
 })
 
 ------------------------------------------------------------------
--- Diagnostic Symbols
+-- Diagnostic Configuration
 ------------------------------------------------------------------
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.HINT] = "󰌵",
+    },
+  },
+})
 
 ------------------------------------------------------------------
 -- Keybindings

5dbce2e2eafe7ecdc8607faa26b1c8511f4c244b
Cainã Costa <me@cfcosta.com>
2025-08-29 11:49:54 -0300

    chore(snacks): remove input field from file explorer

--- plugins/snacks/configuration.lua
+++ plugins/snacks/configuration.lua
@@ -20,13 +20,25 @@ snacks.setup({
       { icon = " ", title = "Projects", section = "projects", padding = 2 },
     },
   },
-  explorer = { enabled = true },
+  explorer = {
+    enabled = true,
+    replace_netrw = true,
+  },
   indent = { enabled = true },
   input = { enabled = true },
   notifier = { enabled = true },
   picker = {
     enabled = true,
-    layout = { preset = "ivy", },
+    layout = { preset = "ivy" },
+    sources = {
+      explorer = {
+        layout = {
+          preset = "sidebar",
+          auto_hide = { "input" },
+          preview = false,
+        },
+      },
+    },
   },
   quickfile = { enabled = true },
   scope = { enabled = true },
@@ -35,24 +47,31 @@ snacks.setup({
   win = {
     input = {
       keys = {
-        ["<Esc>"] = "close"
+        ["<Esc>"] = "close",
       },
     },
   },
   words = { enabled = true },
 })
 
-vim.keymap.set("n", "<leader>/", function() snacks.picker.grep() end,
-  { noremap = true, silent = true, desc = "Snacks: Find in files" })
-vim.keymap.set("n", "<leader>bb", function() snacks.picker.buffers() end,
-  { noremap = true, silent = true, desc = "Snacks: Show Buffers" })
-vim.keymap.set("n", "<leader>gd", function() snacks.git.blame_line() end,
-  { noremap = true, silent = true, desc = "Snacks: Git Blame current Line" })
-vim.keymap.set("n", "<leader>go", function() snacks.gitbrowse() end,
-  { noremap = true, silent = true, desc = "Snacks: Open current line on Github" })
-vim.keymap.set("n", "<leader>op", function() snacks.explorer() end,
-  { noremap = true, silent = true, desc = "Snacks: Toggle file explorer" })
-vim.keymap.set("n", "<leader>xd", function() snacks.picker.diagnostics() end,
-  { noremap = true, silent = true, desc = "Snacks: show diagnostics" })
-vim.keymap.set("n", "<leader>zz", function() snacks.zen() end,
-  { noremap = true, silent = true, desc = "Snacks: Zen mode" })
+vim.keymap.set("n", "<leader>/", function()
+  snacks.picker.grep()
+end, { noremap = true, silent = true, desc = "Snacks: Find in files" })
+vim.keymap.set("n", "<leader>bb", function()
+  snacks.picker.buffers()
+end, { noremap = true, silent = true, desc = "Snacks: Show Buffers" })
+vim.keymap.set("n", "<leader>gd", function()
+  snacks.git.blame_line()
+end, { noremap = true, silent = true, desc = "Snacks: Git Blame current Line" })
+vim.keymap.set("n", "<leader>go", function()
+  snacks.gitbrowse()
+end, { noremap = true, silent = true, desc = "Snacks: Open current line on Github" })
+vim.keymap.set("n", "<leader>op", function()
+  snacks.explorer()
+end, { noremap = true, silent = true, desc = "Snacks: Toggle file explorer" })
+vim.keymap.set("n", "<leader>xd", function()
+  snacks.picker.diagnostics()
+end, { noremap = true, silent = true, desc = "Snacks: show diagnostics" })
+vim.keymap.set("n", "<leader>zz", function()
+  snacks.zen()
+end, { noremap = true, silent = true, desc = "Snacks: Zen mode" })

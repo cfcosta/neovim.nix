231c345951b009d082d4cc1dbb4e03b41142b1d7
Cainã Costa <me@cfcosta.com>
2025-09-23 11:14:01 -0300

    chore(snacks): add missing dependencies

--- plugins/snacks/default.nix
+++ plugins/snacks/default.nix
@@ -1,9 +1,23 @@
-{ inputs, mkPlugin, ... }:
+{
+  inputs,
+  mkPlugin,
+  pkgs,
+  ...
+}:
 let
   inherit (builtins) readFile;
 in
 mkPlugin {
   name = "snacks";
   src = inputs.snacks;
+  inputs = with pkgs; [
+    # snacks.lazygit
+    lazygit
+
+    # snacks.image
+    ghostscript
+    tectonic
+    mermaid-cli
+  ];
   config = readFile ./configuration.lua;
 }

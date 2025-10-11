14daa6681ed5a173eba4c9a32c8aaee73185c5c3
Cainã Costa <me@cfcosta.com>
2025-10-10 21:53:28 -0300

    chore: add search for obsidian notes

--- plugins/obsidian/configuration.lua
+++ plugins/obsidian/configuration.lua
@@ -24,6 +24,16 @@ require("obsidian").setup({
 vim.keymap.set("n", "<leader>n/", "<cmd> Obsidian search<CR>", { desc = "Obsidian: Search notes" })
 vim.keymap.set("n", "<leader>nT", "<cmd> Obsidian tomorrow<CR>", { desc = "Obsidian: Open tomorrow's note" })
 vim.keymap.set("n", "<leader>nd", "<cmd> Obsidian dailies<CR>", { desc = "Obsidian: Find daily notes" })
-vim.keymap.set("n", "<leader>nn", "<cmd> Obsidian quick_switch<CR>", { desc = "Obsidian: Quick switch note" })
 vim.keymap.set("n", "<leader>nt", "<cmd> Obsidian today<CR>", { desc = "Obsidian: Open today's note" })
 vim.keymap.set("n", "<leader>ny", "<cmd> Obsidian yesterday<CR>", { desc = "Obsidian: Open yesterday's note" })
+vim.keymap.set("n", "<leader>ns", "<cmd> Obsidian quick_switch<CR>", { desc = "Obsidian: Quick switch note" })
+
+vim.keymap.set("n", "<leader>nn", function()
+  vim.cmd(("Obsidian dailies %d"):format(-tonumber(vim.fn.strftime("%u"))))
+end, { desc = "Obsidian: search on currently week notes" })
+
+vim.keymap.set("n", "<leader>nN", function()
+  vim.cmd(
+    ("Obsidian dailies %d %d"):format((-tonumber(vim.fn.strftime("%u"))) - 7, (-tonumber(vim.fn.strftime("%u"))) - 1)
+  )
+end, { desc = "Obsidian: search on previous week notes" })

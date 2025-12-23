f0bce79a19ac36baea6a8372ddc5bf4589a6e90a
Cainã Costa <me@cfcosta.com>
2025-12-23 20:28:18 -0300

    fix: use new, rewritten nvim-treesitter

--- plugins/treesitter/configuration.lua
+++ plugins/treesitter/configuration.lua
@@ -1,63 +1,13 @@
-local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
-vim.fn.mkdir(parser_install_dir, "p")
+local install_dir = vim.fn.stdpath("cache") .. "/treesitters"
+vim.fn.mkdir(install_dir, "p")
 
-local new_runtimepath = vim.opt.runtimepath:get()
-table.insert(new_runtimepath, 1, parser_install_dir)
-vim.opt.runtimepath = new_runtimepath
-
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {
-    "agda",
-    "bash",
-    "beancount",
-    "c",
-    "cmake",
-    "comment",
-    "cpp",
-    "css",
-    "csv",
-    "dockerfile",
-    "elixir",
-    "erlang",
-    "git_config",
-    "git_rebase",
-    "gitattributes",
-    "gitcommit",
-    "gitignore",
-    "go",
-    "gomod",
-    "gosum",
-    "html",
-    "hyprlang",
-    "json",
-    "latex",
-    "llvm",
-    "lua",
-    "luau",
-    "markdown",
-    "markdown_inline",
-    "mermaid",
-    "nix",
-    "python",
-    "ruby",
-    "rust",
-    "sql",
-    "ssh_config",
-    "toml",
-    "vim",
-    "vimdoc",
-    "yaml",
-    "zig",
-  },
-  sync_install = false,
-  auto_install = true,
-  highlight = { enable = true },
-  endwise = { enable = true },
-  indent = { enable = true },
-  parser_install_dir = parser_install_dir,
+require("nvim-treesitter").setup({
+  install_dir = install_dir
 })
 
--- Enable folding using treesitter
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false
+-- Enable folding using Treesitter
+vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.wo[0][0].foldmethod = 'expr'
+
+-- Enable indent using Treesitter
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"

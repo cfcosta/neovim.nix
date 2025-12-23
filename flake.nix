ccb3b0a43b189bcfd4da3230ae0fa41b2dd9c3ce
Cainã Costa <me@cfcosta.com>
2025-12-23 17:52:31 -0300

    Revert "chore: remove lean language support"
    This reverts commit ff748dc96820dc436fce0dbe8e8d0f6a8fdc292f.

--- flake.nix
+++ flake.nix
@@ -66,6 +66,10 @@
       url = "github:lewis6991/gitsigns.nvim";
       flake = false;
     };
+    lean = {
+      url = "github:Julian/lean.nvim";
+      flake = false;
+    };
     lspkind = {
       url = "github:onsails/lspkind-nvim";
       flake = false;
---
+++ plugins/lean.nix
@@ -0,0 +1,24 @@
+{
+  inputs,
+  mkPlugin,
+  pkgs,
+  ...
+}:
+mkPlugin {
+  name = "lean";
+  src = inputs.lean;
+
+  inputs = with pkgs; [ lean4 ];
+
+  depends = [
+    "lspconfig"
+    "plenary"
+  ];
+
+  config = ''
+    require('lean').setup{
+      abbreviations = { builtin = true },
+      mappings = true,
+    }
+  '';
+}

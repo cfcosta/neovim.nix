5ff52ef043aa770a5e770be1c4840909079a61e5
Cainã Costa <me@cfcosta.com>
2026-01-13 17:46:50 -0300

    fix: format files

--- plugins/treesitter/configuration.lua
+++ plugins/treesitter/configuration.lua
@@ -2,12 +2,12 @@ local install_dir = vim.fn.stdpath("cache") .. "/treesitters"
 vim.fn.mkdir(install_dir, "p")
 
 require("nvim-treesitter").setup({
-  install_dir = install_dir
+  install_dir = install_dir,
 })
 
 -- Enable folding using Treesitter
-vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.wo[0][0].foldmethod = 'expr'
+vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo[0][0].foldmethod = "expr"
 
 -- Enable indent using Treesitter
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"

15d3e1615606528ed49b87c08eff391dbf9298f6
Cainã Costa <me@cfcosta.com>
2026-02-26 22:02:33 -0300

    fix: remove folding

--- plugins/treesitter/configuration.lua
+++ plugins/treesitter/configuration.lua
@@ -5,9 +5,5 @@ require("nvim-treesitter").setup({
   install_dir = install_dir,
 })
 
--- Enable folding using Treesitter
-vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.wo[0][0].foldmethod = "expr"
-
 -- Enable indent using Treesitter
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"

cbe3f6f07b815fdc6b31aab2a76a67b0dcba9069
Cainã Costa <me@cfcosta.com>
2026-06-10 14:20:29 -0300

    Revert "fix(blink-cmp): skip failing upstream fuzzy-lib tests"
    This reverts commit bbbc3db98149f8409ec03cfaf0fec5c5a081a537.

--- plugins/blink-cmp/default.nix
+++ plugins/blink-cmp/default.nix
@@ -6,24 +6,10 @@
 }:
 let
   inherit (pkgs.stdenv.hostPlatform) system;
-
-  blink-cmp = inputs.blink-cmp.packages.${system}.blink-cmp;
-
-  # Upstream builds the Rust fuzzy matcher (blink-fuzzy-lib) with
-  # rustPlatform.buildRustPackage, which runs `cargo test` in its checkPhase.
-  # Those tests are currently failing upstream, so disable the check on the
-  # nested derivation to let the plugin build.
-  blink-cmp-no-tests = blink-cmp.overrideAttrs (old: {
-    env = old.env // {
-      fuzzy_lib = old.env.fuzzy_lib.overrideAttrs (_: {
-        doCheck = false;
-      });
-    };
-  });
 in
 mkPlugin {
   name = "blink-cmp";
-  src = blink-cmp-no-tests;
+  src = inputs.blink-cmp.packages.${system}.blink-cmp;
 
   depends = [
     "blink-lib"
